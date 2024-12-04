817500713fe26f834712b6287fe1b8f793d432b5
JulioMDH14 <jmunozdehoces@gmail.com>
2024-12-04 17:48:35 +0100

    Delete Ejercicio10.py

--- Ejercicio10.py
+++
@@ -1,9 +0,0 @@
-#10. Juego de «Piedra, papel o tijera»
-
-import random
-
-def elegirOpcionOrdenador():
-    opciones = ["piedra","papel","tijeras"]
-    eleccion = random.choice(opciones)
-    return eleccion
-
