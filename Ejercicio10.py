3e91d8c941cf24ecbdf8b62d356f7ea0116249f8
Julio Muñoz <alu-julmunhoc@foc.es>
2024-12-04 17:48:56 +0100

    Añadido Ejercicio10.py

--- Ejercicio10.py
+++ Ejercicio10.py
@@ -7,3 +7,26 @@ def elegirOpcionOrdenador():
     eleccion = random.choice(opciones)
     return eleccion
 
+def ganadorPartida(usuario, ordenador):
+    if usuario == ordenador:
+        print('EMPATE!!')
+    elif (
+        (usuario == "piedra" and ordenador == "tijera") or
+        (usuario == "papel" and ordenador == "piedra") or
+        (usuario == "tijera" and ordenador == "papel")
+    ):
+        print('Has ganado!!')
+    else:
+        print('Ha ganado el ordenador')
+
+while True:
+    eleccionUsuario = input('Escoge una opción entre piedra, papel o tijeras: ').lower()
+    if eleccionUsuario not in ["piedra","papel","tijeras"]:
+        print('Esa no es una opcion, escoge una válida')
+        continue
+    ordenador = elegirOpcionOrdenador()
+    resultado = ganadorPartida(eleccionUsuario,ordenador)
+    
+    repetir = input("¿Quieres repetir la partida? (Si/NO): ")
+    if repetir.lower() != "si":
+        break
