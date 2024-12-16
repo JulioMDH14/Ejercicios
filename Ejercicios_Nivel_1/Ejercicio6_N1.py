cf6ce810587c133da6dc079cfdde524c6f857e46
Julio Muñoz <alu-julmunhoc@foc.es>
2024-12-16 17:21:10 +0100

    Actualización Ejercicio 6 Nivel 1

--- Ejercicios_Nivel_1/Ejercicio6_N1.py
+++ Ejercicios_Nivel_1/Ejercicio6_N1.py
@@ -2,7 +2,10 @@ asignaturas = ["Matemáticas", "Física", "Química", "Historia", "Lengua"]
 
 suspensas = []
 for asignatura in asignaturas:
-    nota = float(input(f"Introduce la nota que has sacado en {asignatura}: "))
-    if nota < 5:
-        suspensas.append(asignatura)
+    try:
+        nota = float(input(f"Introduce la nota que has sacado en {asignatura}: "))
+        if nota < 5:
+            suspensas.append(asignatura)
+    except ValueError:
+        print("Ese valor no es válido para las notas")
 print(f"Debes repetir las asignaturas de {suspensas}")
