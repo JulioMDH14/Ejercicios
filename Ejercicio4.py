136d6afa15815c1787ee2aa7ded8eb689164399c
JulioMDH14 <jmunozdehoces@gmail.com>
2024-12-16 16:13:15 +0100

    Delete Ejercicio4.py

--- Ejercicio4.py
+++
@@ -1,3 +0,0 @@
-asigntaturas = ["Matemáticas","Física","Química","Historia","Lengua"]
-for asignatura in asigntaturas:
-    print(f"Yo estudio {asignatura}, donde {asignatura} es cada una de las asignaturas de la lista")
