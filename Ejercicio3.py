41f6991b5d75f95bf4df72ecab4ea75aff148d9a
JulioMDH14 <jmunozdehoces@gmail.com>
2024-12-16 10:38:24 +0100

    Añadidos Ejercicios.py 3,4,5,6,8 de Nivel 1

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -1,13 +1,3 @@
-#3. Adivina un número (fácil)
-
-numero_oculto = 14
-pregunta = int(input('¿Cuál es el número oculto? '))
-#Un intento
-intento = 1
-while intento != 0:
-    if pregunta == numero_oculto:
-        print('¡Has acertado!')
-        break
-    else:
-        print('No has acertado')
-    intento -= 1
+asigntaturas = ["Matemáticas","Física","Química","Historia","Lengua"]
+for asignatura in asigntaturas:
+    print(asignatura)
--- Ejercicio4.py
+++ Ejercicio4.py
@@ -1,11 +1,3 @@
-#4. Creando Listas y Agregando Elementos
-
-lista = [1,2,3,4,5,6]
-lista.pop()
-lista.append('Elemento agregado')
-lista.insert(3,14)
-
-lista2 = lista.copy()
-print(f'Lista2 es {lista2}')
-
-print(lista)
+asigntaturas = ["Matemáticas","Física","Química","Historia","Lengua"]
+for asignatura in asigntaturas:
+    print(f"Yo estudio {asignatura}, donde {asignatura} es cada una de las asignaturas de la lista")
